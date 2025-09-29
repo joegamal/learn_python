bfe17aea4890bea7e2dca70a95a933683f5c6b61
joegamal <yusufmagamalth@gmail.com>
2025-09-29 23:55:22 +0300

    loops in raw python

---
+++ loops.py
@@ -0,0 +1,21 @@
+#loops in python
+
+#for loops
+
+for i in 10:
+    print(i)
+
+
+for i in range(5):
+    print(i)
+
+
+student = {"name": "Yusuf", "age": 21}
+for key, value in student.items():
+    print(key, ":", value)
+
+
+while i < 15:
+    print(f'pc is now on instruction {i}')
+
+    
