2a36fc0522dea8baee5e9c1f14416ef26ff76912
joegamal <yusufmagamalth@gmail.com>
2025-09-30 00:06:07 +0300

    basic functions in python

---
+++ functions.py
@@ -0,0 +1,17 @@
+##functions in python
+
+#normal function
+
+def greet(name):
+    print(f"hello {name}")
+
+
+greet("yusuf")
+
+#lambda functions for knowledge only
+
+
+#yields key word for knowledge only 
+
+
+#function as a parameter for knowledge only 
