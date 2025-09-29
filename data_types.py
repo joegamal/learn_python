6d5c9e9a1010b03441ff5aab95313dd7a45dca4e
joegamal <yusufmagamalth@gmail.com>
2025-09-29 23:50:19 +0300

    data types and a preview on data structure

---
+++ data_types.py
@@ -0,0 +1,43 @@
+#primitives
+
+##the data types of python are 
+
+#int 
+x_int = 5
+
+#float
+X_float = 5.5
+
+#boolean
+x_bool = True
+
+#string
+x_string = "string"
+
+#####################################
+
+#sequence types
+
+#list
+x_list = []
+
+
+#tuple -> set in java
+x_tuple = ()
+
+
+#sequence
+x_sequnece = range(5)
+
+
+#set
+x_set = {}
+
+#dictionary -> json
+x_dictionary = {
+    "name": "yusuf",
+    "age": 21
+}
+
+
+
